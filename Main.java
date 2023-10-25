a707ec1ffc753b136b8bde0d45e2101de92e91f3
Ancellist <83424461+Ancellist@users.noreply.github.com>
2023-10-25 09:44:35 +0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);//Создание ввода
+        int n = scanner.nextInt();//Ввод переменной
+        int m = scanner.nextInt();//Ввод переменной
+        int[][] arr = new int[n][m];//2-х мерный массив
+
+        for (int i = 0 ;i < arr.length;i++ ){
+            for (int j = 0;j<arr[i].length; j++){
+                arr[i][j]=scanner.nextInt();
+            }
+        }//Цикл ввода матрицы построчено
+
+        for (int i = 0 ;i < arr.length;i++ ){
+            for (int j = 0;j<arr[i].length; j++){
+                System.out.println(arr[j][i]);
+            }
+            System.out.println();//Пробел между столбиками
+        }//Цикл вывода матрицы по столбцам
+    }
+}
