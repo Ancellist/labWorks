4540ba22a511814dc0912753aafa539cc538ce56
Ancellist <83424461+Ancellist@users.noreply.github.com>
2023-10-25 13:19:12 +0400

    Add files via upload

--- Matrix.java
+++ Matrix.java
@@ -7,6 +7,10 @@ public class Matrix {
         int n = scanner.nextInt();//Ввод переменной
         int m = scanner.nextInt();//Ввод переменной
         int[][] arr = new int[n][m];//2-х мерный массив
+        int[] arr1 = new int[n];
+        int[] arr2 = new int[n];
+        int buff = 0;
+        int buff1 = 0;
 
         for (int i = 0 ;i < arr.length;i++ ){
             for (int j = 0;j<arr[i].length; j++){
@@ -15,13 +19,24 @@ public class Matrix {
         }//Цикл ввода матрицы построчено
 
         for (int i =0;i < arr.length;i++){
+            arr1[i]= arr[i][i];
             System.out.println(arr[i][i]);
         }//Цикл вывода диагонали
 
         for (int i =0;i < arr.length;i++){
-            int j = arr.length-1;
+            int j = arr.length-i-1;
             System.out.println(arr[i][j]);
+            arr2[i] = arr[i][j];
         }//Цикл вывода побочной диагонали
 
+        for (int i =0;i<arr1.length;i++){
+            buff += arr1[i];
+            System.out.println(buff);
+        }
+
+        for (int i =0;i<arr2.length;i++){
+            buff1 += arr2[i];
+            System.out.println(buff1);
+        }
     }
 }
